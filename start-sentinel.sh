d410a8becb132ce29f8ec2c0d2ceead859c9a11a
sapiens83 <sapiens83@gmail.com>
2016-05-02 15:38:14 -0500

    Update start-sentinel.sh

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -11,7 +11,8 @@ SENTINEL_CONFIGURATION_FILE=/etc/sentinel.conf
 
 # Host and port we will listen for requests on
 echo "bind 0.0.0.0" > $SENTINEL_CONFIGURATION_FILE
-echo "port 6379" >> $SENTINEL_CONFIGURATION_FILE
+#echo "port 6379" >> $SENTINEL_CONFIGURATION_FILE
+echo "port ${ANNOUNCED_PORT}" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel monitor $GROUP_NAME $MASTER_IP $MASTER_PORT $QUORUM" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel down-after-milliseconds $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel failover-timeout $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
