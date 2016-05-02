d5cdfd68dc4fb023d58043607997090a14385644
sapiens83 <sapiens83@gmail.com>
2016-05-02 16:20:14 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -9,9 +9,9 @@ REDIS_CONFIGURATION_FILE=/etc/redis.conf
 echo "bind 127.0.0.1" > $REDIS_CONFIGURATION_FILE
 #if [ -n "${MASTER_IP}" ] && [ -n "${MASTER_PORT}" ]; then
 
-  echo "port ${ANNOUNCED_PORT}" >> $REDIS_CONFIGURATION_FILE
+  #echo "port ${ANNOUNCED_PORT}" >> $REDIS_CONFIGURATION_FILE
 #else
-  #echo "port 6379" >> $REDIS_CONFIGURATION_FILE
+  echo "port 6379" >> $REDIS_CONFIGURATION_FILE
 #fi
 echo "dir ." >> $REDIS_CONFIGURATION_FILE
 
