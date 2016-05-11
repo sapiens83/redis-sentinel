c9204ff5352300630da8a9a7162e7d32c7578da2
sapiens83 <sapiens83@gmail.com>
2016-05-11 17:23:10 -0500

    Update start-sentinel.sh

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -8,7 +8,7 @@ MASTER_IP=${MASTER_IP}
 #MASTER_PORT=${MASTER_PORT}
 while true
 do
-MASTER_PORT=$(redis-cli -h 9.7.116.148 -p 6378 info server | grep tcp_port | cut -f2 -$
+MASTER_PORT=$(redis-cli -h 9.7.116.148 -p 6378 info server | grep tcp_port | cut -f2 -d :)
         if [ "MASTER_PORT" != "" ]; then
                 break
         fi
