6b22107035863e86880198fb0693c317b74f2980
sapiens83 <sapiens83@gmail.com>
2016-05-13 16:20:34 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -39,3 +39,7 @@ if [ -n "${MASTER_IP}" ] && [ -n "${MASTER_PORT}" ]; then
 fi
 
 /usr/local/bin/redis-server $REDIS_CONFIGURATION_FILE
+if [ "$ROLE"=="master"];then
+        ip=$(hostname -I | cut -f1 -d " ")
+        redis-cli -h 9.7.116.148 -p 6378 set master $ip
+fi
