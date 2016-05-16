5e2f427ffee26ea342820b2e212e3f9f8da63788
sapiens83 <sapiens83@gmail.com>
2016-05-16 12:29:41 -0500

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -2,7 +2,7 @@
 
 ANNOUNCED_PORT=${ANNOUNCED_PORT}
 ROLE=${ROLE}
-#MASTER_IP=${MASTER_IP}
+MASTER_IP="9.7.116.148"
 #MASTER_PORT=${MASTER_PORT}
 
 # getiing the master info
@@ -10,9 +10,9 @@ while true
 do
 MASTER_PORT=$(redis-cli -h 9.7.116.148 -p 6378 info server | grep tcp_port | cut -f2 -d :)
         if [ "$MASTER_PORT" != "" ]; then
-                MASTER_IP=$(redis-cli -h 9.7.116.148 -p 6378 MONITOR & sleep 2 ; kill $!)
-                MASTER_IP=$(echo -e  "$MASTER_IP"|cut -f2 -d[ | cut -f2 -d " "| cut -f1 -d: | tail -n 1  )
-                echo $MASTER_IP
+                #MASTER_IP=$(redis-cli -h 9.7.116.148 -p 6378 MONITOR & sleep 2 ; kill $!)
+                #MASTER_IP=$(echo -e  "$MASTER_IP"|cut -f2 -d[ | cut -f2 -d " "| cut -f1 -d: | tail -n 1  )
+                #echo $MASTER_IP
                 echo $MASTER_PORT
                 break
         fi
