e5e98765f7411a50c0f1ad66a52fdc9ecf70aad2
sapiens83 <sapiens83@gmail.com>
2016-05-16 15:07:12 -0500

    Update start-sentinel.sh

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -6,18 +6,18 @@ set -e
 GROUP_NAME=${GROUP_NAME}
 #MASTER_IP=${MASTER_IP}
 MASTER_IP="9.7.116.148"
-#MASTER_PORT=${MASTER_PORT}
-while true
-do
-MASTER_PORT=$(redis-cli -h 9.7.116.148 -p 6378 info server | grep tcp_port | cut -f2 -d :)
-        if [ "$MASTER_PORT" != "" ]; then
+MASTER_PORT=${MASTER_PORT}
+#while true
+#do
+#MASTER_PORT=$(redis-cli -h 9.7.116.148 -p 6378 info server | grep tcp_port | cut -f2 -d :)
+ #       if [ "$MASTER_PORT" != "" ]; then
                 #MASTER_IP=$(redis-cli -h 9.7.116.148 -p 6378 MONITOR & sleep 10 ; kill $!)
                 #MASTER_IP=$(echo -e  "$MASTER_IP"|cut -f2 -d[ | cut -f2 -d " "| cut -f1 -d: | tail -n 1 )
                 #echo $MASTER_IP
-                echo $MASTER_PORT
-                break
-        fi
-done
+  #              echo $MASTER_PORT
+   #             break
+    #    fi
+#done
 QUORUM=${QUORUM}
 #ANNOUNCED_IP=${ANNOUNCED_IP}
 ANNOUNCED_PORT=${ANNOUNCED_PORT}
@@ -31,6 +31,11 @@ echo "sentinel monitor $GROUP_NAME $MASTER_IP $MASTER_PORT $QUORUM" >> $SENTINEL
 echo "sentinel down-after-milliseconds $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel failover-timeout $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel parallel-syncs $GROUP_NAME 1" >> $SENTINEL_CONFIGURATION_FILE
+
+echo "sentinel monitor cluster2 $MASTER_IP 6381 $QUORUM" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel down-after-milliseconds cluster2 5000" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel failover-timeout cluster2 5000" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel parallel-syncs cluster2 1" >> $SENTINEL_CONFIGURATION_FILE
 #echo "sentinel announce-ip ${ANNOUNCED_IP}" >> $SENTINEL_CONFIGURATION_FILE
 #echo "sentinel announce-port ${ANNOUNCED_PORT}" >> $SENTINEL_CONFIGURATION_FILE
 #echo "sentinel known-slave redis ${ANNOUNCED_IP} ${ANNOUNCED_PORT}" >> $SENTINEL_CONFIGURATION_FILE
